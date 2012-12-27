79884d012c915fe1c68d57ca4fb7dc23cb1fd563
James Meldrum <jrm.general@gmail.com>
2012-12-27 14:34:48 -0500

    Update squirrel.py

--- squirrel.py
+++ squirrel.py
@@ -1,8 +1,3 @@
-# Squirrel Eat Squirrel (a 2D Katamari Damacy clone)
-# By Al Sweigart al@inventwithpython.com
-# http://inventwithpython.com/pygame
-# Released under a "Simplified BSD" license
-
 import random, sys, time, math, pygame
 from pygame.locals import *
 
